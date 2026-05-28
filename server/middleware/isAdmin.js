ef1f8d4a950d4c6896b28890fb94e4c3e6031c60
Habiba Mahmoud <Habiba2403019@miuegypt.edu.eg>
2026-05-28 17:53:31 +0300

    updated user model

--- server/middleware/isAdmin.js
+++ server/middleware/isAdmin.js
@@ -5,7 +5,7 @@
 
 module.exports = function isAdmin(req, res, next) {
   if (!req.session.user) {
-    return res.redirect("/login");
+    return res.redirect("/auth/login");
   }
   if (req.session.user.role !== "admin") {
     return res.status(403).send("Access denied. Admins only.");
