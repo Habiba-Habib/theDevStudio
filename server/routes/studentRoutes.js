d1ce7f157e76610aa511247cbb91ed4bcf6f3ef4
Habiba Mahmoud <Habiba2403019@miuegypt.edu.eg>
2026-05-29 16:54:20 +0300

    updated

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const studentController = require("../controllers/studentController");
-
+const session = require('express-session');
 
 router.get("/dashboard", studentController.getDashboard);
 router.get("/leaderboard", studentController.getLeaderboard);
