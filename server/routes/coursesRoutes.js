c58a1c4e60489cb1290987f6bd2facc141b779b2
nadineashmawey <nadinetamer318@gmail.com>
2026-05-29 05:38:49 +0300

    fixed coursesRoute to show instructor name and avatar in course details page

--- server/routes/coursesRoutes.js
+++ server/routes/coursesRoutes.js
@@ -3,24 +3,6 @@ const router = express.Router();
 const Course = require('../models/Course');
 const mongoose = require('mongoose');
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(404).render('public/page-404');
-    }
-
-    const course = await Course.findById(req.params.id);
-
-    if (!course) {
-      return res.status(404).render('public/page-404');
-    }
-
-    res.render('guest/course-description', { course });
-  } catch (err) {
-    next(err);
-  }
-});
-
 router.get('/all-courses', async (req, res, next) => {
   try {
     const courses = await Course.find({ isPublished: true });
@@ -32,11 +14,22 @@ router.get('/all-courses', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const course = await Course.findById(req.params.id);
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(404).render('public/page-404');
+    }
+
+    const course = await Course.findById(req.params.id)
+    .populate("instructor", "name avatar");
+
+    if (!course) {
+      return res.status(404).render('public/page-404');
+    }
+
     res.render('guest/course-description', { course });
   } catch (err) {
     next(err);
   }
 });
+
 
 module.exports = router;
