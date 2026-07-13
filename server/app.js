dfd8d80c5cbcc23173444cd6c7b34425bb644135
Habiba Mahmoud <Habiba2403019@miuegypt.edu.eg>
2026-07-14 00:07:55 +0300

    updated

--- server/app.js
+++ server/app.js
@@ -92,6 +92,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
